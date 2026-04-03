6e060a6d3032b269b2d885ff7ea42b1f6ab69b33
Wallace Soares <wallace.soares@incognia.com>
2026-04-02 22:09:19 -0300

    Initial Commit

---
+++ Paletta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Paletta
+//
+//  Created by Wallace Soares on 02/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Paletta/PalettaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalettaApp.swift
+//  Paletta
+//
+//  Created by Wallace Soares on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PalettaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
